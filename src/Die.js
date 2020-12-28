67ac1c8ea7ebf1b67c64045d496a366fdb2a8957
Ricky <ricky@MacBook-Air-3.fritz.box>
2020-12-28 01:37:52 +0100

    animation added

--- src/Die.js
+++ src/Die.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 import './Die.css';
 
 class Die extends Component {
-    render(){
-        return <i className={`Die fas fa-dice-${this.props.face}`}></i>;
+    render() {
+        return (
+            <i
+                className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && "shaking"}`}
+            />
+        );
     }
 }
 export default Die;
